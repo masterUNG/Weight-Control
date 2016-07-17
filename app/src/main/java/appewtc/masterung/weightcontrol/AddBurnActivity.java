5734c89ebe92c91b13333fba58ee620cf8c0167b
masterUNG <phrombutr@gmail.com>
2016-07-17 14:50:12 +0700

    Show Date on BurnAdd Finish

--- app/src/main/java/appewtc/masterung/weightcontrol/AddBurnActivity.java
+++ app/src/main/java/appewtc/masterung/weightcontrol/AddBurnActivity.java
@@ -2,12 +2,23 @@ package appewtc.masterung.weightcontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AddBurnActivity extends AppCompatActivity {
+
+    //Explicit
+    private String dateString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_burn);
-    }
-}
+
+        dateString = getIntent().getStringExtra("Date");
+        TextView textView = (TextView) findViewById(R.id.txtBurnAdd);
+        textView.setText("Date = " + dateString);
+
+
+    }   // Main Method
+
+}   // Main Class
