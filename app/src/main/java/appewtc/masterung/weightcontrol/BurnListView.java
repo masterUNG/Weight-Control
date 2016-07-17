04d5e7bd6703638dcb5353e5321d4c36e6b07c1f
masterUNG <phrombutr@gmail.com>
2016-07-17 14:41:25 +0700

    Start Create AddBurn

--- app/src/main/java/appewtc/masterung/weightcontrol/BurnListView.java
+++ app/src/main/java/appewtc/masterung/weightcontrol/BurnListView.java
@@ -1,13 +1,39 @@
 package appewtc.masterung.weightcontrol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class BurnListView extends AppCompatActivity {
+
+    //Explicit
+    private String dateString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_burn_list_view);
+
+        dateString = getIntent().getStringExtra("Date");
+
+        TextView textView = (TextView) findViewById(R.id.txtBurnList);
+        textView.setText("Date = " + dateString);
+
+
+    }   // Main Method
+
+    public void clickAddBurn(View view) {
+
+        Intent intent = new Intent(BurnListView.this, AddBurnActivity.class);
+        intent.putExtra("Date", dateString);
+        startActivity(intent);
+
     }
-}
+
+    public void clickBackBurn(View view) {
+        finish();
+    }
+
+}   // Main Class
