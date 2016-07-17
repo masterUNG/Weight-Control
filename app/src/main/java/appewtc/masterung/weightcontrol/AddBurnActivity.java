a94f5d2fea32afd0b842a7caaa33412d04a5bc0d
masterUNG <phrombutr@gmail.com>
2016-07-17 15:01:45 +0700

    Show ListBurn finish

--- app/src/main/java/appewtc/masterung/weightcontrol/AddBurnActivity.java
+++ app/src/main/java/appewtc/masterung/weightcontrol/AddBurnActivity.java
@@ -1,7 +1,10 @@
 package appewtc.masterung.weightcontrol;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class AddBurnActivity extends AppCompatActivity {
@@ -17,6 +20,29 @@ public class AddBurnActivity extends AppCompatActivity {
         dateString = getIntent().getStringExtra("Date");
         TextView textView = (TextView) findViewById(R.id.txtBurnAdd);
         textView.setText("Date = " + dateString);
+
+        //Create ListView
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM exerciseTABLE", null);
+        cursor.moveToFirst();
+        String[] exerciseStrings = new String[cursor.getCount()];
+        String[] burnStrings = new String[cursor.getCount()];
+
+        for (int i=0;i<cursor.getCount();i+=1) {
+
+            exerciseStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_exercise));
+            burnStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_burn));
+            cursor.moveToNext();
+
+        }   // for
+        cursor.close();
+        MyAdapter myAdapter = new MyAdapter(this, exerciseStrings, burnStrings);
+        ListView listView = (ListView) findViewById(R.id.livBurnAdd);
+        listView.setAdapter(myAdapter);
+
+        
+
 
 
     }   // Main Method
